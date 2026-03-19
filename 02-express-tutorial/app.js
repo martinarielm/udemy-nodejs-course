fe9566707338a009e4d88044110033b3065e0676
Martín Mármol <martinmarmol.ar@gmail.com>
2026-03-19 11:50:58 -0300

    Implement basic Express server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  console.log("Request received");
+  res.end("Hello World");
+});
+
+server.listen(5000);
