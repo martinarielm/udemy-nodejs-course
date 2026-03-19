b828a8d5bd3b5771ef41c9ec2ca77aff3b65a06d
Martín Mármol <martinmarmol.ar@gmail.com>
2026-03-19 12:38:26 -0300

    Add basic server with routing and html homepage

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,24 @@
 import http from "node:http";
+import { readFileSync } from "node:fs";
+
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
-  console.log("Request received");
-  res.end("Hello World");
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Server About Page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
