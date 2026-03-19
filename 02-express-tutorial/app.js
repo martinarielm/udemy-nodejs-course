7d5a41410e24ae49ac601fca8c4da70742b3158d
Martín Mármol <martinmarmol.ar@gmail.com>
2026-03-19 17:55:48 -0300

    Create minimal Express server and routing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,23 @@
-import http from "node:http";
-import { readFileSync } from "node:fs";
+import express from "express";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const homePage = readFileSync("./index.html");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
+const app = express();
+const PORT = 5000;
 
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Server About Page</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Page Not Found</h1>");
-    res.end();
-  }
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
-server.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
