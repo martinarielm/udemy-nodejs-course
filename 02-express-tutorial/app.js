40fb78b026ddc182a95dfc473a16f47136ec6f2e
Martín Mármol <martinmarmol.ar@gmail.com>
2026-03-21 18:05:23 -0300

    Refactor logger middleware and restore home route with logging

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { products } from "./data.js";
 
 // import path, { dirname } from "node:path";
 // import { fileURLToPath } from "node:url";
@@ -9,51 +8,19 @@ import { products } from "./data.js";
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  return res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
-  // res.json(products);
-});
+const logger = (req, res, next) => {
+  const { method, url } = req;
+  const time = new Date().toLocaleTimeString();
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
+  console.log("👨‍🎤 -> url:", url);
+  console.log("👨‍🎤 -> method:", method);
+  console.log("👨‍🎤 -> time:", time);
 
-  return res.json(newProducts);
-});
+  next();
+};
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find(
-    (product) => product.id === Number(req.params.id),
-  );
-
-  if (!product) {
-    return res.status(404).json("Product not found");
-  }
-
-  return res.json(product);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.startsWith(search),
-    );
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (!sortedProducts.length) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-
-  return res.status(200).json(sortedProducts);
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
 app.listen(PORT, () => {
