1c09eac32cff73121d26a0a818be55ec942ed650
Martín Mármol <martinmarmol.ar@gmail.com>
2026-04-14 19:29:39 -0300

    Add error handle middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import tasksRouter from "./routes/tasks.js";
 import connectToDatabase from "./db/connect.js";
 import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
 
 const PORT = 3000;
 const app = expresss();
@@ -13,6 +14,7 @@ app.use(expresss.json());
 
 app.use("/api/v1/tasks", tasksRouter);
 
+app.use(errorHandler);
 app.use(notFound);
 
 const startServer = async () => {
