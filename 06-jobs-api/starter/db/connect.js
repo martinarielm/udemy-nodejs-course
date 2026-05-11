c2c2530568a391a3fd75bf5ba54c013667ba179c
Martín Mármol <martinmarmol.ar@gmail.com>
2026-05-11 13:03:35 -0300

    Update mongoose version and add auth and jobs routes

--- 06-jobs-api/starter/db/connect.js
+++ 06-jobs-api/starter/db/connect.js
@@ -1,12 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+// const connectDB = (url) => {
+//   return mongoose.connect(url, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   });
+// };
 
-module.exports = connectDB
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // useUnifiedTopology: true,
+};
+
+const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}`;
+
+const connectToDatabase = async (url) => {
+  try {
+    await mongoose.connect(mongoUri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = { connectToDatabase };
