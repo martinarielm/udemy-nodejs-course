6d4a6bc3c6262d33b5d12770dcf6130319935cc3
Martín Mármol <martinmarmol.ar@gmail.com>
2026-05-29 15:39:35 -0300

    Add security middlewares

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,9 +2,19 @@ require("dotenv").config();
 require("express-async-errors");
 const morgan = require("morgan");
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const app = express();
+
 app.disable("x-powered-by");
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // connectDB
 // const { connectDB } = require("./db/connect");
