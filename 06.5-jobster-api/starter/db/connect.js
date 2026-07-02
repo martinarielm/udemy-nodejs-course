a8c117769b3993f49f00105a72c67981ce740c32
Martín Mármol <martinmarmol.ar@gmail.com>
2026-07-02 14:52:10 -0300

    Refactor database connection and update package names for consistency

--- 06.5-jobster-api/starter/db/connect.js
+++ 06.5-jobster-api/starter/db/connect.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
-module.exports = connectDB
+const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}`;
+
+const connectToDatabase = async (url) => {
+  try {
+    await mongoose.connect(mongoUri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = { connectToDatabase };
